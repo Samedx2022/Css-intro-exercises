328d6f33cc444f3a74b320409927b6a06b3ddfe6
Samedx2022 <116844571+Samedx2022@users.noreply.github.com>
2023-02-14 21:34:20 -0500

    Add files via upload

---
+++ DOMM/dom.js
@@ -0,0 +1,43 @@
+
+    // EXERCISE 1
+//document.getElementById("container")
+
+// EXERCISE 2
+// document.querySelector('#container')
+
+// EXERCISE 3
+// document.querySelectorAll('.second')
+
+// EXERCISE 4
+// document.querySelector('ol .third')
+
+// EXERCISE 5
+// cont.innerText = "Hello!"
+
+// EXERCISE 6
+//let footer = document.querySelector(".footer");
+//footer.classList.add("main");
+
+// EXERCISE 7
+//let footer = document.querySelector(".footer");
+//footer.classList.remove("main");
+
+ // EXERCISE 8
+// const newEle = document.createElement('li')
+
+// Exercise 9
+//newEle.textContent = 'four'
+
+// EXERCISE 10
+//let list = document.querySelector("ul");
+// list.appendChild(li);
+
+// EXERCISE 11
+//let LIOL = document.querySelectorAll("ol li");
+
+//for(let i = 0; i < LIOL.length; i++){
+//    LIOL[i].style.backgroundColor = "green"; }
+
+// EXERCISE 12
+//let foot = document.querySelector(".footer");
+//foot.remove();
