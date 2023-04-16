92ce27ce33eae6c55cf0ae6ea7f99fd5295513f3
Samedx2022 <116844571+Samedx2022@users.noreply.github.com>
2023-04-15 21:23:03 -0400

    Part | DOM practice and Part || Movie app!

---
+++ JqueryExer/jQueryExe.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+    console.log("Lets gett ready to party with jQUERY!!!! YESSIIRRRR")
+});
+
+$('img').addClass("image-center");
+
+$("p").eq(5).remove();
+
+/// Next code below Represents the font size of the h1!, We can see the actual number in the console! ///
+
+$(document).ready(function () {
+    let number = Math.floor(Math.random() * 100)
+    console.log("H1 new Font-Size is:", number)
+    $('h1').css("font-size", number)
+});
+
+$('ol').append("<li> Just adding a new Item!");
+
+$('ol').empty();
+
+$('ol').append("<p> I would like to apologize for the previous list!")
+
+$("#button1").click(function () {
+    let r = $("input").eq(0).val();
+    let g = $("input").eq(1).val();
+    let b = $("input").eq(2).val();
+
+    const color = "rgb(" + r + "," + g + "," + b + ")";
+    $("body").css("background-color", color)
+});
+
+$('img').on('click', function () {
+    $(this).fadeOut(1000,function () {
+        $(this).remove()
+    })
+})
---
+++ JqueryExer/movieapp.js
@@ -0,0 +1,18 @@
+$(document).ready(function () {
+    console.log("We are ready baby!");
+    $('form').submit(function(event) {
+        event.preventDefault();
+        let movie = $('#movie').val();
+        console.log("Title:", movie)
+        let rating = $('#rating').val();
+        console.log("Rating:", rating)
+        $('#4ner').append('<div id="Final">' + movie + '--' + rating + '<button class="removeButton">Remove</button></div>');
+    
+        $('#movie').val('');
+        $('#rating').val('');
+    });
+    
+    $(document).on('click', '.removeButton', function() {
+        $(this).parent().remove();
+    });
+});
